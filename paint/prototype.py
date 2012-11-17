96738b5847a80d3dc9d88eb041fd5639392432fb
= <=>
2012-11-17 23:40:21 +0800

    prototype.py

--- paint/prototype.py
+++ paint/prototype.py
@@ -21,10 +21,12 @@ times = [ 200*((row[0] + row[1]/12.0) - 2007)-110 for row in data ]
 predLine = PolyLine( zip( times, pred ), strokeColor = colors.blue )
 highLine = PolyLine( zip( times, high ), strokeColor = colors.red )
 lowLine = PolyLine( zip( times, low ), strokeColor = colors.green )
+title = String( 60, 110, 'Sunspots', fontsize=14, fillColor=colors.blue )
 #add
 drawing.add( predLine )
 drawing.add( highLine )
 drawing.add( lowLine )
+drawing.add( title )
 #save to file
 renderPDF.drawToFile( drawing, 'prototype.pdf', 'Report of weater in \
 	special aspect' )
