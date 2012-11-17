0a303dc0db40c3998fa25b93e525230201a98ebc
= <=>
2012-11-17 20:46:28 +0800

    dictionary

---
+++ map/dict.py
@@ -0,0 +1,7 @@
+people = ['fanyiwei', 'liudepeng', 'wangbaoquan', 'kangtiantian']
+numbers = ['2234', '434', '3', '656']
+phonebook = {}
+for p in people:
+	phonebook[p] = numbers[people.index(p)]
+print phonebook
+
